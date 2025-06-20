43c48a7d9c946fd5cad0e57e590d41fd98e34040
Omkar-203 <omkaradsare2003@gmail.com>
2025-06-20 22:50:26 +0530

    Initial commit

---
+++ front-end/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from  "react";
+
+const Footer=()=>{
+    return(
+        <div class="footer">
+            <h3>E-comm Dashboard</h3>
+        </div>
+    )
+}
+
+export default Footer;
